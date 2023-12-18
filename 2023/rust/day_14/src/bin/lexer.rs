65a94fa04cb7c00d38986e5ef06b746bd57c4bff
rodmedeiross <roddmedeiross@gmail.com>
2023-12-18 20:23:38 -0300

    day_14 part02

--- 2023/rust/day_14/src/bin/lexer.rs
+++ 2023/rust/day_14/src/bin/lexer.rs
@@ -1,18 +1,14 @@
 // use itertools::{repeat_n, Itertools};
-use std::{collections::BTreeMap, str::FromStr, task::Wake};
+use indicatif::ProgressIterator;
+use std::{
+    collections::BTreeMap,
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 #[derive(Debug, Default)]
 pub struct RockSchema {
-    rocks: BTreeMap<(usize, usize), RockType>,
     rocks_vec: Vec<Vec<char>>,
-}
-
-#[derive(Debug, Default)]
-pub enum RockType {
-    Rounded,
-    Cube,
-    #[default]
-    Empty,
 }
 
 fn print_schema(schema: &Vec<Vec<char>>) {
@@ -22,46 +18,234 @@ fn print_schema(schema: &Vec<Vec<char>>) {
     });
 }
 
+#[derive(Debug, Default, Eq)]
+pub struct State {
+    step: usize,
+    grid: Vec<Vec<char>>,
+}
+
+impl State {
+    fn new(step: usize, grid: Vec<Vec<char>>) -> Self {
+        Self { step, grid }
+    }
+}
+
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        self.grid == other.grid
+    }
+}
+
+impl std::hash::Hash for State {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.grid.hash(state);
+    }
+}
+
+fn north_rollout(
+    original_map: &Vec<Vec<char>>,
+    output_map: &mut Vec<Vec<char>>,
+    boundaries: usize,
+    cache: &mut HashMap<String, Vec<Vec<char>>>,
+) {
+    let hash = output_map
+        .clone()
+        .into_iter()
+        .map(|linha| {
+            linha
+                .into_iter()
+                .map(|ch| ch.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>()
+        .join("");
+
+    if let Some(cache_value) = cache.get(&hash) {
+        *output_map = cache_value.to_vec();
+    }
+
+    (0..boundaries).for_each(|_| {
+        original_map.iter().enumerate().for_each(|(y, row)| {
+            row.into_iter()
+                .enumerate()
+                .for_each(|(x, _)| match output_map[y][x] {
+                    '.' if y < output_map.len() - 1 && output_map[y + 1][x] == 'O' => {
+                        output_map[y + 1][x] = '.';
+                        output_map[y][x] = 'O'
+                    }
+                    _ch => (),
+                });
+        });
+    });
+
+    cache.insert(hash, output_map.clone());
+}
+
+fn west_rollout(
+    original_map: &Vec<Vec<char>>,
+    output_map: &mut Vec<Vec<char>>,
+    boundaries: usize,
+    cache: &mut HashMap<String, Vec<Vec<char>>>,
+) {
+    let hash = output_map
+        .clone()
+        .into_iter()
+        .map(|linha| {
+            linha
+                .into_iter()
+                .map(|ch| ch.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>()
+        .join("");
+
+    if let Some(cache_value) = cache.get(&hash) {
+        *output_map = cache_value.to_vec();
+    }
+
+    (0..boundaries).for_each(|_| {
+        original_map.iter().enumerate().for_each(|(y, row)| {
+            row.into_iter()
+                .enumerate()
+                .for_each(|(x, _)| match output_map[y][x] {
+                    '.' if x < output_map[0].len() - 1 && output_map[y][x + 1] == 'O' => {
+                        output_map[y][x + 1] = '.';
+                        output_map[y][x] = 'O'
+                    }
+                    _ch => (),
+                });
+        });
+    });
+
+    cache.insert(hash, output_map.clone());
+}
+
+fn south_rollout(
+    original_map: &Vec<Vec<char>>,
+    output_map: &mut Vec<Vec<char>>,
+    boundaries: usize,
+    cache: &mut HashMap<String, Vec<Vec<char>>>,
+) {
+    let hash = output_map
+        .clone()
+        .into_iter()
+        .map(|linha| {
+            linha
+                .into_iter()
+                .map(|ch| ch.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>()
+        .join("");
+
+    if let Some(cache_value) = cache.get(&hash) {
+        *output_map = cache_value.to_vec();
+    }
+
+    (0..boundaries).for_each(|_| {
+        original_map.iter().enumerate().for_each(|(y, row)| {
+            row.into_iter()
+                .enumerate()
+                .for_each(|(x, _)| match output_map[y][x] {
+                    'O' if y < output_map.len() - 1 && output_map[y + 1][x] == '.' => {
+                        output_map[y + 1][x] = 'O';
+                        output_map[y][x] = '.'
+                    }
+                    _ch => (),
+                });
+        });
+    });
+    cache.insert(hash, output_map.clone());
+}
+
+fn east_rollout(
+    original_map: &Vec<Vec<char>>,
+    output_map: &mut Vec<Vec<char>>,
+    boundaries: usize,
+    cache: &mut HashMap<String, Vec<Vec<char>>>,
+) {
+    let hash = output_map
+        .clone()
+        .into_iter()
+        .map(|linha| {
+            linha
+                .into_iter()
+                .map(|ch| ch.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>()
+        .join("");
+
+    if let Some(cache_value) = cache.get(&hash) {
+        *output_map = cache_value.to_vec();
+    }
+
+    (0..boundaries).for_each(|_| {
+        original_map.iter().enumerate().for_each(|(y, row)| {
+            row.into_iter()
+                .enumerate()
+                .for_each(|(x, _)| match output_map[y][x] {
+                    'O' if x < output_map[0].len() - 1 && output_map[y][x + 1] == '.' => {
+                        output_map[y][x + 1] = 'O';
+                        output_map[y][x] = '.'
+                    }
+                    _ch => (),
+                });
+        });
+    });
+
+    cache.insert(hash, output_map.clone());
+}
+
 impl RockSchema {
     pub fn rolllouutttt(&self) -> usize {
         let mut rocks = self.rocks_vec.clone();
+        let y_length = rocks.len() - 1;
+        let x_length = rocks[0].len() - 1;
+        let mut cache: HashMap<String, Vec<Vec<char>>> = HashMap::new();
+        let mut result_cache = HashSet::new();
 
-        (0..rocks.len() - 1).for_each(|_| {
-            self.rocks_vec
-                .clone()
-                .into_iter()
-                .enumerate()
-                .for_each(|(y, row)| {
-                    row.into_iter()
-                        .enumerate()
-                        .for_each(|(x, _)| match rocks[y][x] {
-                            '.' if y < rocks.len() - 1 && rocks[y + 1][x] == 'O' => {
-                                rocks[y + 1][x] = '.';
-                                rocks[y][x] = 'O'
-                            }
-                            _ch => (),
-                        });
-                });
-        });
-        // dbg!(print_schema(&rocks));
+        for iter in 0..1_000_000_000 {
+            result_cache.insert(State::new(iter, rocks.clone()));
 
-        let count = rocks
-            .iter()
-            .enumerate()
-            .map(|(index, lines)| {
-                let mult = rocks.len() - index;
-                let rounded_count = lines
+            north_rollout(&self.rocks_vec, &mut rocks, y_length, &mut cache);
+            west_rollout(&self.rocks_vec, &mut rocks, x_length, &mut cache);
+            south_rollout(&self.rocks_vec, &mut rocks, y_length, &mut cache);
+            east_rollout(&self.rocks_vec, &mut rocks, x_length, &mut cache);
+
+            if let Some(state) = result_cache.get(&State::new(0, rocks.to_owned())) {
+                let cycle_len = iter + 1 - state.step;
+                let remaining = 1_000_000_000 - iter - 1;
+                let remaining = remaining % cycle_len;
+
+                for _ in 0..remaining {
+                    north_rollout(&self.rocks_vec, &mut rocks, y_length, &mut cache);
+                    west_rollout(&self.rocks_vec, &mut rocks, x_length, &mut cache);
+                    south_rollout(&self.rocks_vec, &mut rocks, y_length, &mut cache);
+                    east_rollout(&self.rocks_vec, &mut rocks, x_length, &mut cache);
+                }
+
+                let count = rocks
                     .iter()
-                    .filter_map(|x| if *x == 'O' { Some(x) } else { None })
-                    .count();
+                    .enumerate()
+                    .map(|(index, lines)| {
+                        let mult = rocks.len() - index;
+                        let rounded_count = lines
+                            .iter()
+                            .filter_map(|x| if *x == 'O' { Some(x) } else { None })
+                            .count();
 
-                mult * rounded_count
-            })
-            .collect::<Vec<_>>();
-
-        // dbg!(&count);
-
-        count.iter().sum()
+                        mult * rounded_count
+                    })
+                    .collect::<Vec<_>>();
+                return count.iter().sum();
+            }
+        }
+        panic!("So lonng")
     }
 }
 
@@ -69,27 +253,11 @@ impl FromStr for RockSchema {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let rocks =
-            s.lines()
-                .enumerate()
-                .flat_map(|(index, line)| {
-                    line.trim().chars().enumerate().into_iter().flat_map(
-                        move |(inner_index, ch)| match ch {
-                            ch if ch == 'O' => Some(((inner_index, index), RockType::Rounded)),
-                            ch if ch == '#' => Some(((inner_index, index), RockType::Cube)),
-                            _ => Some(((inner_index, index), RockType::Empty)),
-                        },
-                    )
-                })
-                .collect::<BTreeMap<(usize, usize), RockType>>();
-
         let rocks_vec = s
             .lines()
             .map(|line| line.trim().chars().collect::<Vec<char>>())
             .collect::<Vec<Vec<char>>>();
 
-        // dbg!(&rocks, &rocks_vec);
-
-        Ok(RockSchema { rocks, rocks_vec })
+        Ok(RockSchema { rocks_vec })
     }
 }
---
+++ 2023/rust/day_14/src/bin/part02.rs
@@ -0,0 +1,37 @@
+use crate::lexer::*;
+mod lexer;
+
+fn main() {
+    let data = include_str!("input_part01.txt");
+    let result = process_data(data);
+    println!("Result: {}", result);
+}
+
+fn process_data(data: &str) -> usize {
+    data.parse::<RockSchema>().unwrap().rolllouutttt()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(
+        "O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....",
+        "64"
+    )]
+    fn should_return_sum_of_path_walk(#[case] input: &str, #[case] expected: &str) {
+        let result = process_data(input).to_string();
+        assert_eq!(result, expected);
+    }
+}
