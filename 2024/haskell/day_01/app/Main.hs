3b32e6adbeca3a2601f3082b0fd1a12bf846ab84
Rodrigo Medeiros <roddmedeiross@gmail.com>
2024-12-02 23:10:09 -0300

    part 02, day_01 haskell

--- 2024/haskell/day_01/app/Main.hs
+++ 2024/haskell/day_01/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Data.List (sort)
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable (Hashable)
+import Data.List (foldl', sort)
 
 processFile :: String -> ([Int], [Int])
 processFile contents =
@@ -14,10 +16,32 @@ sumList :: ([Int], [Int]) -> Int
 sumList (first, second) =
   sum (zipWith (\x y -> abs (x - y)) first second)
 
+countOccurrences :: (Eq k, Hashable k) => [k] -> HM.HashMap k Int
+countOccurrences = foldr (\x acc -> HM.insertWith (+) x 1 acc) HM.empty
+
+sumOccurrences :: ([Int], [Int]) -> Int
+sumOccurrences (first, second) =
+  let firstC = countOccurrences first
+      secondC = countOccurrences second
+
+      result =
+        sum $
+          map
+            ( \n ->
+                let count1 = HM.lookupDefault 0 n firstC
+                    count2 = HM.lookupDefault 0 n secondC
+                 in n * count1 * count2
+            )
+            (HM.keys firstC)
+   in result
+
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
   let (first, second) = processFile contents
   let sumL = sumList (first, second)
 
+  let occurrences = sumOccurrences (first, second)
+
   print sumL
+  print occurrences
