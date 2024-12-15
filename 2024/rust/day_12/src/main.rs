ab5d4e58ae4425b404997f84a68d0244739186f9
Rodrigo Medeiros <roddmedeiross@gmail.com>
2024-12-15 01:29:20 -0300

    day 12 part 01

--- 2024/rust/day_12/src/main.rs
+++ 2024/rust/day_12/src/main.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
@@ -13,51 +12,85 @@ fn process_part01(i: &str) -> usize {
         .map(|line| line.chars().map(|ch| ch).collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    let mut queue: VecDeque<(isize, isize)> = VecDeque::new();
-    let mut map: HashMap<char, HashSet<(usize, usize)>> = HashMap::new();
+    let mut regions: Vec<HashMap<char, HashSet<(usize, usize)>>> = Vec::new();
+    let mut saw: HashSet<(usize, usize)> = HashSet::new();
 
     for (y, line) in grid.iter().enumerate() {
-        for (x, ch) in line.iter().enumerate() {
-            queue.push_back((x as isize, y as isize));
-            map.entry(*ch)
-                .or_insert(HashSet::new())
-                .insert((x as usize, y as usize));
+        for (x, &ch) in line.iter().enumerate() {
+            if saw.contains(&(x, y)) {
+                continue;
+            }
 
-            while queue.len() > 0 {
-                if let Some((qx, qy)) = queue.pop_back() {
-                    for (nx, ny) in vec![(qx - 1, qy), (qx, qy + 1), (qx + 1, qy), (qx, qy - 1)] {
-                        if nx < 0
-                            || ny < 0
-                            || nx >= grid[0].len() as isize
-                            || ny >= grid.len() as isize
-                        {
-                            continue;
-                        }
+            saw.insert((x, y));
+            let mut queue = VecDeque::from([(x as isize, y as isize)]);
+            let mut region: HashMap<char, HashSet<(usize, usize)>> = HashMap::new();
 
-                        if grid[ny as usize][nx as usize] != *ch {
-                            continue;
-                        }
+            region.entry(ch).or_insert_with(HashSet::new).insert((x, y));
 
-                        if let Some(saw) = map.get(ch) {
-                            if saw.contains(&(nx as usize, ny as usize)) {
-                                continue;
-                            }
-                        }
-
-                        map.entry(*ch)
-                            .or_insert(HashSet::new())
-                            .insert((nx as usize, ny as usize));
-
-                        queue.push_front((nx, ny))
+            while let Some((qx, qy)) = queue.pop_front() {
+                for (nx, ny) in [(qx - 1, qy), (qx, qy + 1), (qx + 1, qy), (qx, qy - 1)] {
+                    if !(0..grid[0].len() as isize).contains(&nx)
+                        || !(0..grid.len() as isize).contains(&ny)
+                    {
+                        continue;
                     }
+
+                    let (nx, ny) = (nx as usize, ny as usize);
+
+                    if grid[ny][nx] != ch || saw.contains(&(nx, ny)) {
+                        continue;
+                    }
+
+                    region
+                        .entry(ch)
+                        .or_insert_with(HashSet::new)
+                        .insert((nx, ny));
+
+                    saw.insert((nx, ny));
+                    queue.push_back((nx as isize, ny as isize));
                 }
             }
+
+            regions.push(region);
         }
     }
 
-    dbg!(&map, &per);
+    let areas: Vec<usize> = regions
+        .iter()
+        .flat_map(|region| region.values().map(|set| set.len()))
+        .collect();
 
-    21
+    let mut perimeters: Vec<usize> = Vec::new();
+
+    for sets in regions.iter() {
+        for (&k, vs) in sets.iter() {
+            let mut perimeter = 0;
+
+            for &(x, y) in vs {
+                for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                    let nx = x as isize + dx;
+                    let ny = y as isize + dy;
+
+                    if nx < 0
+                        || ny < 0
+                        || nx >= grid[0].len() as isize
+                        || ny >= grid.len() as isize
+                        || grid[ny as usize][nx as usize] != k
+                    {
+                        perimeter += 1;
+                    }
+                }
+            }
+
+            perimeters.push(perimeter);
+        }
+    }
+
+    areas
+        .iter()
+        .zip(perimeters.iter())
+        .map(|(a, b)| a * b)
+        .sum()
 }
 
 #[cfg(test)]
